fbf0b0093dc3954aedd965c055475bb0390561f3
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-09-05 15:42:21 +0300

    Finish: lab 1

--- w1-d1-lab1/app/src/main/java/fi/metropolia/helloworld/MainActivity.kt
+++ w1-d1-lab1/app/src/main/java/fi/metropolia/helloworld/MainActivity.kt
@@ -2,11 +2,26 @@ package fi.metropolia.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var isHello = true
+
+    private val clickListener = View.OnClickListener {
+        isHello = !isHello
+
+        when (isHello) {
+            true  -> textTitle.text = getString(R.string.title_main)
+            false -> textTitle.text = getString(R.string.title_secondary)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggleTextButton.setOnClickListener(clickListener)
     }
 }
