ec6e9c5d63129f0d28a2af49abdfbe44899228a5
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-09-05 16:21:47 +0300

    Update: retain state over orientation changes

--- w1-d1-lab1/app/src/main/java/fi/metropolia/helloworld/MainActivity.kt
+++ w1-d1-lab1/app/src/main/java/fi/metropolia/helloworld/MainActivity.kt
@@ -7,21 +7,36 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var isHello = true
+    private var isHelloWorld = true
 
-    private val clickListener = View.OnClickListener {
-        isHello = !isHello
-
-        when (isHello) {
-            true  -> textTitle.text = getString(R.string.title_main)
-            false -> textTitle.text = getString(R.string.title_secondary)
-        }
+    companion object {
+        private const val TOGGLE_STATE_KEY = "TOGGLE_STATE_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toggleTextButton.setOnClickListener(clickListener)
+        toggleTextButton.setOnClickListener {
+            isHelloWorld = !isHelloWorld
+            updateTitle(isHelloWorld)
+        }
+
+        if (savedInstanceState != null) {
+            isHelloWorld = savedInstanceState.getBoolean(TOGGLE_STATE_KEY)
+            updateTitle(isHelloWorld)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putBoolean(TOGGLE_STATE_KEY, isHelloWorld)
+    }
+
+    private fun updateTitle (toggleState: Boolean) {
+        when (toggleState) {
+            true  -> textTitle.text = getString(R.string.title_main)
+            false -> textTitle.text = getString(R.string.title_secondary)
+        }
     }
 }
