74ec9c1b45048228472bba3ab5ee5d7d1be61fbb
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-09-09 13:38:46 +0300

    Finish: mood app

--- w1-d1-fragments-lab_1/app/src/main/java/fi/metropolia/mood/MainActivity.kt
+++ w1-d1-fragments-lab_1/app/src/main/java/fi/metropolia/mood/MainActivity.kt
@@ -1,12 +1,41 @@
 package fi.metropolia.mood
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+private const val MOOD_GOOD = "GOOD"
+private const val MOOD_BAD = "BAD"
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, MoodFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.activity_main)
+        goodMoodButton.setOnClickListener(this)
+        badMoodButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            goodMoodButton -> navigateToMoodFragment(MOOD_GOOD)
+            badMoodButton  -> navigateToMoodFragment(MOOD_BAD)
+        }
+    }
+
+    private fun navigateToMoodFragment(mood: String) {
+        val fragment = MoodFragment.newInstance(mood)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction.addToBackStack(null)
+        transaction.add(R.id.fragmentContainer, fragment, "MOOD_FRAGMENT").commit()
+        supportFragmentManager.executePendingTransactions()
+    }
+
+    override fun onFragmentInteraction() {
+        onBackPressed()
     }
 }
---
+++ w1-d1-fragments-lab_1/app/src/main/java/fi/metropolia/mood/MoodFragment.kt
@@ -0,0 +1,107 @@
+package fi.metropolia.mood
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.content.Context
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_mood.view.*
+
+private const val USER_MOOD = "USER_MOOD"
+private const val MOOD_GOOD = "GOOD"
+
+class MoodFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var userMood: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            userMood = it.getString(USER_MOOD)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_mood, container, false)
+
+        val isGoodMood = (userMood == MOOD_GOOD)
+
+        val text = if (isGoodMood) getString(R.string.mood_good) else getString(R.string.mood_bad)
+
+        val color = if (isGoodMood) resources.getColor(R.color.yellow, activity?.theme)
+        else resources.getColor(R.color.blue, activity?.theme)
+
+        val secondaryColor = if (isGoodMood) resources.getColor(R.color.yellowDark, activity?.theme)
+        else resources.getColor(R.color.blueDark, activity?.theme)
+
+        val buttonBg = if (isGoodMood) R.drawable.button_good_goback else R.drawable.button_bad_goback
+
+        val imageSrc = if (isGoodMood) R.mipmap.good_face_ic else R.mipmap.bad_face_ic
+
+        view.moodImage.setImageResource(imageSrc)
+        view.moodFragment.setBackgroundColor(color)
+        view.moodText.text = text
+        view.moodText.setTextColor(secondaryColor)
+        view.goBackButton.setBackgroundResource(buttonBg)
+        view.goBackButton.setTextColor(secondaryColor)
+        view.goBackButton.setOnClickListener{ onGoBackButtonPressed() }
+
+        return view
+    }
+
+    private fun onGoBackButtonPressed() {
+        listener?.onFragmentInteraction()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction()
+    }
+
+    companion object {
+        fun newInstance(userMood: String) =
+            MoodFragment().apply {
+                arguments = Bundle().apply {
+                    putString(USER_MOOD, userMood)
+                }
+            }
+    }
+
+    private fun setupViewElements(v: View, mood: String?) {
+        val isGoodMood = (mood == MOOD_GOOD)
+        val color = if (isGoodMood)
+            resources.getColor(R.color.yellow, activity?.theme)
+            else resources.getColor(R.color.blue, activity?.theme)
+        val secondaryColor = if (isGoodMood) resources.getColor(R.color.yellowDark, activity?.theme)
+            else resources.getColor(R.color.blueDark, activity?.theme)
+        val buttonBg = if (isGoodMood) resources.getDrawable(R.drawable.button_good_goback, activity?.theme)
+            else resources.getDrawable(R.drawable.button_bad_goback, activity?.theme)
+        val imageSrc = if (isGoodMood) R.mipmap.good_face_ic else R.mipmap.bad_face_ic
+        v.moodImage.setImageResource(imageSrc)
+        v.moodFragment.setBackgroundColor(color)
+//        v.moodText.textView.setTextColor(secondaryColor)
+        v.goBackButton.background = buttonBg
+        v.goBackButton.setTextColor(secondaryColor)
+    }
+}
