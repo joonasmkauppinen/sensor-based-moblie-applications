de81de85958024ad7b2e317261c466245c4a33e2
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-09-18 17:50:20 +0300

    AR Core fix and camera lab 1

--- w4-d1-arcore-lab_2/app/src/main/java/fi/metropolia/arcore/MainActivity.kt
+++ w4-d1-arcore-lab_2/app/src/main/java/fi/metropolia/arcore/MainActivity.kt
@@ -1,9 +1,11 @@
 package fi.metropolia.arcore
 
+import android.graphics.Point
 import android.os.Bundle
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.animation.ModelAnimator
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import com.google.ar.sceneform.rendering.ModelRenderable
@@ -12,12 +14,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val MODEL_NAME = "Buu_FinalMesh.sfb"
-        const val TAILS_MODEL = "CH_UgandaTails_Sketchfab.sfb"
-        const val BOOK_MODEL = "Book_scan.sfb"
+        const val STORMTROOPER_MODEL = "silly_dancing.sfb"
+        const val MINECRAFT_CHEST_MODEL = "scene.sfb"
+        const val UGANDA_TAILS_MODEL = "UgandaTails.sfb"
+        const val THANOS_MODEL = "Thanos.sfb"
     }
 
     private var myRenderable: ModelRenderable? = null
+    lateinit var modelAnimator: ModelAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +30,17 @@ class MainActivity : AppCompatActivity() {
         val fragment = arFragment as ArFragment
 
         ModelRenderable.builder()
-            .setSource(fragment.context, Uri.parse(BOOK_MODEL))
+            .setSource(fragment.context, Uri.parse(THANOS_MODEL))
             .build()
-            .thenAccept{ myRenderable = it }
+            .thenAccept{
+                myRenderable = it
+                modelAnimator = ModelAnimator(
+                    myRenderable!!.getAnimationData(0),
+                    myRenderable)
+                modelAnimator.repeatCount = ModelAnimator.INFINITE
+                modelAnimator.start()
+            }
+
 
         fragment.setOnTapArPlaneListener { hitResult, _, _ ->
             if (myRenderable == null) return@setOnTapArPlaneListener
@@ -39,6 +51,24 @@ class MainActivity : AppCompatActivity() {
             viewNode.setParent(anchorNode)
             viewNode.renderable = myRenderable
             viewNode.select()
+
+//            val point = getScreenCenter()
+//            val hits: List<HitResult> = fragment.arSceneView.arFrame!!
+//                .hitTest(point.x.toFloat(), point.y.toFloat())
+//            for (hit in hits) {
+//                val trackable = hit.trackable
+//                if (trackable is Plane) {
+//
+////                    ModelAnimator(myRenderable?.getAnimationData(0), myRenderable)
+////                        .start()
+//                }
+//            }
+
         }
+    }
+
+    private fun getScreenCenter(): Point {
+        val v = arFragment.view!!
+        return Point(v.width / 2, v.height / 2)
     }
 }
